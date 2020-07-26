0ab20a8948bb4865774fe112c617740adcf87685
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-07-26 17:49:39 +0300

    create dynamic submenu

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'home'
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    same_products = []
+    submenu = [
+        {'href': 'products_all', 'name': 'all'},
+        {'href': 'products_home', 'name': 'home'},
+        {'href': 'products_office', 'name': 'office'},
+        {'href': 'products_furniture', 'name': 'furniture'},
+        {'href': 'products_modern', 'name': 'modern'},
+        {'href': 'products_classic', 'name': 'classic'},
+
+    ]
+    context = {
+        'title': 'products',
+        'same-products': same_products,
+        'submenu': submenu
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'contacts'
+    }
+    return render(request, 'mainapp/contact.html', context)
