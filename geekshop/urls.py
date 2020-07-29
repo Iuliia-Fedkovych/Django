bafdf8f6a9405a378632173017b1f7ab6925f708
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-07-29 19:57:07 +0300

    setup project for working with mediafiles

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 import mainapp.views as mainapp
 
 urlpatterns = [
@@ -32,3 +35,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
