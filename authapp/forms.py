3d376c4e410e25bc016c20a22156babdf3266ad8
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-02 22:50:34 +0300

    correct mistake in auth/forms

--- authapp/forms.py
+++ authapp/forms.py
@@ -39,7 +39,7 @@ class ShopUserRegisterForm(UserCreationForm):
 class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
-        fields = ('username', 'first_name', 'password', 'email', 'age')
+        fields = ('username', 'first_name', 'password', 'email', 'age', 'avatar')
 
     def __init__(self, *args, **kwargs):
         super(ShopUserEditForm, self).__init__(*args, **kwargs)
