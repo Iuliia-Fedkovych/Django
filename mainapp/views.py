819e7b4794a7cd96eb084b0ddfad6a5ec7490d15
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-04 22:03:36 +0300

    implement products sort by category create basketapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,6 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
-
 
 
 def main(request):
@@ -15,11 +14,28 @@ def main(request):
 
 def products(request, pk=None):
     print(pk)
-    products = Product.objects.all()[:3]
+    title = 'products'
     categories = ProductCategory.objects.all()
 
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'all'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+        context = {
+            'title': title,
+            'products': products,
+            'categories': categories,
+            'category': category,
+        }
+        return render(request, 'mainapp/products_list.html', context)
+
+    products = Product.objects.all()[:3]
+
     context = {
-        'title': 'products',
+        'title': title,
         'products': products,
         'categories': categories
     }
