b6047d0278a2e979d64ece42939cd120a1bbabec
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-11 20:18:37 +0300

    basket edit

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 from basketapp.models import Basket
@@ -39,3 +41,26 @@ def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        basket_item = Basket.objects.get(pk=pk)
+
+        if quantity > 0:
+            basket_item.quantity = quantity
+            basket_item.save()
+        else:
+            basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user)
+        print(basket_items)
+        context = {
+            'basket_items': basket_items,
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html', context)
+
+        return JsonResponse({'result': result})
