ee0f3729a488eeec72fc77d06acdf4a88087e418
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-16 13:01:10 +0300

    add pagination to product list

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
+    path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
     path('product/<int:pk>/', mainapp.product, name='product')
 
 ]
