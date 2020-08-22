0c3f2fa7c350b47a267c550098fa0c28be1be962
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-22 18:23:41 +0300

    user activation

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
@@ -5,8 +7,33 @@ from django.urls import reverse
 from authapp.forms import ShopUserLoginForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.forms import ShopUserEditForm
+from authapp.models import ShopUser
 
 
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
+    title = f'Account confirmation {user.username}'
+
+    message = f'To confirm your account {user.username} on the {settings.DOMAIN_NAME} use following link:' \
+              f'{settings.DOMAIN_NAME}{verify_link}'
+
+    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, activation_key):
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expired():
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return render(request, 'authapp/verification.html')
+        else:
+            print(f'error activation user: {user}')
+            return render(request, 'authapp/verification.html')
+    except Exception as e:
+        print(f'error activation user: {e.args}')
+        return HttpResponseRedirect(reverse('main'))
 
 
 def login(request):
@@ -46,8 +73,13 @@ def register(request):
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
 
         if register_form.is_valid():
-            register_form.save()
-            return HttpResponseRedirect(reverse('auth:login'))
+            user = register_form.save()
+            if send_verify_mail(user):
+                print('Confirmation message sent')
+                return HttpResponseRedirect(reverse('auth:login'))
+            else:
+                print('Confirmation message send error')
+                return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
 
