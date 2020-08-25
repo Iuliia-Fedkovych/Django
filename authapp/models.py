6878f98fa970a711edfbe79311d1a638d10811e9
Iuliia-Fedkovych <Julia_Fedkovich@mail.ru>
2020-08-25 22:31:40 +0300

    add authorization using VK

--- authapp/models.py
+++ authapp/models.py
@@ -7,7 +7,9 @@ from django.utils.timezone import now
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveSmallIntegerField(verbose_name='age')
+    # age = models.PositiveSmallIntegerField(verbose_name='age')
+    age = models.PositiveSmallIntegerField(verbose_name='age', default=18)
+
 
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
